69edfbe478a6d00e0db0fd2410151cdc9744ec57
Frank Mertens <frank@cyblogic.de>
2010-01-16 07:46:12 +0100

    Adding first two color palette.

---
+++ colors/Classic.js
@@ -0,0 +1,23 @@
+charcoal.colors["Classic"] =
+{
+	displayName: "Classic",
+	colors: {
+		"Black"        : "#000000",
+		"Red"          : "#B21616",
+		"Green"        : "#16B216",
+		"Yellow"       : "#B26816",
+		"Blue"         : "#1616B2",
+		"Magenta"      : "#B216B2",
+		"Cyan"         : "#16B2B2",
+		"White"        : "#B2B2B2",
+		"BrightBlack"  : "#686868",
+		"BrightRed"    : "#FF5454",
+		"BrightGreen"  : "#54FF54",
+		"BrightYellow" : "#FFFF54",
+		"BrightBlue"   : "#5454FF",
+		"BrightMagenta": "#FF54FF",
+		"BrightCyan"   : "#54FFFF",
+		"BrightWhite"  : "#FFFFFF",
+		"Selection"    : "#4C80FF80"
+	}
+};
---
+++ colors/Default.js
@@ -0,0 +1,23 @@
+charcoal.colors["Default"] =
+{
+	displayName: "Black on White",
+	colors: {
+		"Black"        : "#FFFFFF",
+		"Red"          : "#800000",
+		"Green"        : "#008000",
+		"Yellow"       : "#808000",
+		"Blue"         : "#000080",
+		"Magenta"      : "#800080",
+		"Cyan"         : "#008080",
+		"White"        : "#000000",
+		"BrightBlack"  : "#808080",
+		"BrightRed"    : "#FF0000",
+		"BrightGreen"  : "#00FF00",
+		"BrightYellow" : "#FFFF00",
+		"BrightBlue"   : "#0000FF",
+		"BrightMagenta": "#FF00FF",
+		"BrightCyan"   : "#00FFFF",
+		"BrightWhite"  : "#404040",
+		"Selection"    : "#4C80FF80"
+	}
+};
