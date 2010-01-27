a61c9e038808339580ccb9d03e4333323cdee6eb
Frank Mertens <frank@cyblogic.de>
2010-01-27 15:17:53 +0100

    Reversing a stupid workaround.

--- syntax/plain.js
+++ syntax/plain.js
@@ -1,12 +1,5 @@
 charcoal.syntax["plain"] = function()
 {
-	DEFINE("Line", FIND(CHAR('\n')));
-	
-	DEFINE("PlainText",
-		REPEAT(
-			REF("Line")
-		)
-	);
-	
+	DEFINE("PlainText");
 	ENTRY("PlainText");
 }
